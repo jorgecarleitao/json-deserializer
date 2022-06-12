f3ecbfa5ae2c3ae14086e9f52181f13d1af7adff
Chandra Penke <16369152+ncpenke@users.noreply.github.com>
2022-06-12 14:17:39 -0700

    Improved performance of parsing string (both escaped and in-place) (#7)

--- benches/parse.rs
+++ benches/parse.rs
@@ -2,9 +2,19 @@ use std::io::Read;
 
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
+mod serde_mapping;
+
 fn parse_serde_json(data: &[u8]) {
     let a: serde_json::Value = serde_json::from_slice(data).unwrap();
     assert!(a.is_array());
+}
+
+fn parse_serde_json_typed(data: &[u8]) {
+    let a: serde_mapping::Value = serde_json::from_slice(data).unwrap();
+    if let serde_mapping::Value::Array(_) = a {
+    } else {
+        panic!()
+    }
 }
 
 fn parse_simd_json(data: &mut [u8]) {
@@ -29,15 +39,16 @@ fn read(file: &str, log2_size: usize) -> Vec<u8> {
 }
 
 fn add_benchmark(c: &mut Criterion) {
-    for type_ in [
-        "integer",
-        "float",
-        "string",
-        "bool",
-        "object_string",
-        "object_bool",
-    ] {
-        (10..=20usize).step_by(2).for_each(|log2_size| {
+    (10..=20usize).step_by(2).for_each(|log2_size| {
+        for type_ in [
+            "integer",
+            "float",
+            "string",
+            "string_escaped_chars",
+            "bool",
+            "object_string",
+            "object_bool",
+        ] {
             let bytes = read(type_, log2_size);
 
             c.bench_function(
@@ -49,6 +60,11 @@ fn add_benchmark(c: &mut Criterion) {
                 b.iter(|| parse_serde_json(&bytes))
             });
 
+            c.bench_function(
+                &format!("{} serde_json_custom 2^{}", type_, log2_size),
+                |b| b.iter(|| parse_serde_json_typed(&bytes)),
+            );
+
             let bytes = bytes.clone();
             c.bench_function(&format!("{} simd_json 2^{}", type_, log2_size), move |b| {
                 b.iter_batched(
@@ -57,8 +73,8 @@ fn add_benchmark(c: &mut Criterion) {
                     BatchSize::SmallInput,
                 )
             });
-        })
-    }
+        }
+    })
 }
 
 criterion_group!(benches, add_benchmark);
